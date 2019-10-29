8daa02f969c98ba671b6f215ff264838a32cf038
jimjunker1 <james.junker1@gmail.com>
2019-10-29 17:55:40 -0500

    update and build.

--- R/standalone.R
+++ R/standalone.R
@@ -1,3 +1,35 @@
 # small standalone functions
 
 '%ni%' <- Negate('%in%')
+
+mutate_cond <- function(.data, condition, ..., envir = parent.frame()) {
+  condition <- eval(substitute(condition), .data, envir)
+  .data[condition, ] <- .data[condition, ] %>% mutate(...)
+  .data
+}
+
+C_to_overKT <- function(a){1/(8.61733*10^-5*a)}
+overkt_to_C <- function(a){1/(a*(8.61733*10^-5)) - 273.15}
+C_to_overkt_stand <- function(a,b){(1/(8.61733e-5*(b+273.15)) - (1/(8.61733e-5*(a+273.15))))}
+
+na.rm_mean <- function(...,na.rm=FALSE){mean(c(...),na.rm=na.rm)}
+
+rmap <- function (.x, .f, ...) {
+  if(is.null(dim(.x))) stop("dim(X) must have a positive length")
+  .x <- t(.x) %>% as.data.frame(.,stringsAsFactors=F)
+  purrr::map(.x=.x,.f=.f,...)
+}
+
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+get_legend<-function(myggplot){
+  tmp <- ggplot_gtable(ggplot_build(myggplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
+
+hello <- print('olleh')
