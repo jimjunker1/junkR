2fbffafe7783b300bb2b77e77ee39632f6e36bf4
jimjunker1 <james.junker1@gmail.com>
2020-05-16 12:35:22 -0500

    updated with author_exchange function

---
+++ R/author_exchange.R
@@ -0,0 +1,14 @@
+#' This function exchanges full author list formated with First and Middle initials
+#' and full family name. This allows the creation of bib entries with full author
+#' lists
+#'
+#' @param x bib entry created from RefManageR::ReadGS() (i.e., list) with incomplete
+#' author list
+#' @param y personList created from convert_complete_author(). Same length as x
+#' @example mapply(author_exchange, x = jrj.bib, y = full_authors_list)
+#' @export
+author_exchange <- function(x,y){
+  x_flat <- unlist(x)
+  x_flat$author <- y
+  return(RelistBibEntry(x_flat))
+}
--- R/convert_complete_author.R
+++ R/convert_complete_author.R
@@ -10,14 +10,14 @@ convert_complete_author <- function(complete_coauthor_string){
 
   x <- complete_coauthor_string
   author_lists <- lapply(strsplit(x, ","), stringr::str_trim)
-  author_lists <- lapply(author_lists, function(x) sapply(x, strsplit, " "))
-  author_persons <- lapply(author_lists, function(x) lapply(x, function(name){
+  author_lists <- sapply(author_lists, strsplit, " ")
+  author_persons <- lapply(author_lists, function(name){
     if(length(name) == 2){
       person(given = substr(name[1], start = 1, stop = 1), family = name[2], middle = NULL)
     } else if(length(name) == 3){
-      person(given = substr(name[1],start =1, stop = 1), family = name[3], middle = substr(name[2], start = 1, stop = 1))
+      person(given = paste0(substr(name[1],start =1, stop = 1),substr(name[2], start = 1, stop = 1)), family = name[3])
     } else{
-      person(given = name[1], family = length(name), middle = paste0(substr(name[2:(length(name)-1)], start = 1, stop = 1)))
-    }}))
-
+      person(given = paste0(substr(name[1:(length(name)-1)], start = 1, stop = 1)), family = length(name))
+    }})
+  return(as.personList(author_persons))
 }
